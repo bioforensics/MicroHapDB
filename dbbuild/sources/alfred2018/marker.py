89fd85bf48468402ca0b40f8526ab344fa3c5b3f
Daniel Standage <daniel.standage@gmail.com>
2019-10-09 22:33:35 -0400

    Finish ALFRED marker preprocessing

--- dbbuild/sources/alfred2018/marker.py
+++ dbbuild/sources/alfred2018/marker.py
@@ -13,10 +13,6 @@ from re import findall, search
 import sys
 
 
-Marker = namedtuple('Marker', 'markerid,label,markername,chrom,start,end,variants')
-Variant = namedtuple('Variant', 'dbsnpid,chrom,position')
-
-
 def parse_markers_from_table(filename):
     if not os.path.exists(filename):
         return list()
@@ -65,70 +61,28 @@ def alfred_marker_detail_scrape(instream):
         message += ', only found {:d}'.format(len(matches))
         raise ValueError(message)
     dbsnpids = [m[0] for m in matches]
-    return dbsnpids
+    match = search('(mh\d+(KK|CP|NK)-\d+)', data)
+    assert match, data
+    name = match.group(1)
+    return name, dbsnpids
 
 
-def alfred_loci_prelim(freqstream, coordstream):
-    locusids = dict()
-    for locusid, locusname in alfred_locus_list(freqstream):
-        locusids[locusname] = locusid
-    next(coordstream)
-    for line in coordstream:
-        values = line.strip().split()
-        locusid = locusids[values[0]]
-        yield [*values[1:], 'ALFRED', locusid, values[0]]
-
-
-def lovd_loci_prelim(instream):
-    for line in instream:
-        if line.startswith(' '):
-            continue
-        values = line.strip().split(',')
-        offsets = [int(o) for o in values[-1].split(':')]
-        start = int(values[2])
-        values[2] = start + offsets[0]
-        values[3] = start + offsets[-1] + 1
-        yield [*values[1:4], 'doi:10.1016/j.fsigen.2018.05.008', values[0]]
-
-
-def linköping_loci_prelim(defstream, varstream):
-    coords = dict()
-    for line in varstream:
+def alfred_marker_coords(vcf, mapping):
+    rsidcoords = dict()
+    for line in vcf:
         if line.startswith('#'):
             continue
-        chrnum, pos, rsid, *data = line.split('\t')
-        chrom = 'chr' + chrnum
-        coords[rsid] = (chrnum, int(pos) - 1)
+        chrnum, posstr, rsid, *values = line.strip.split()
+        chrom = chrnum if chrnum.startswith('chr') else 'chr' + chrnum
+        pos = int(posstr) - 1
+        rsidcoords[rsid] = (chrom, pos)
 
-    microhaps = defaultdict(list)
-    for line in defstream:
-        if not line.startswith('rs'):
+    for line in mapping:
+        if line.startswith('Name'):
             continue
-        rsid, label = line.strip().split('\t')
-        microhaps[label].append(rsid)
-
-    for label, snplist in microhaps.items():
-        loclist = [coords[rsid] for rsid in snplist]
-        chrom = set([loc[0] for loc in loclist])
-        assert len(chrom) == 1, chrom
-        chromstr = 'chr{:s}'.format(list(chrom)[0])
-        coordlist = [loc[1] for loc in loclist]
-        start = min(coordlist)
-        end = max(coordlist) + 1
-        yield chromstr, start, end, 'ISFG2019:P597', label
-
-
-def combine_loci(alfredstream, lovdstream, linkstream):
-    loci = list()
-    for varstream in (alfredstream, lovdstream, linkstream):
-        next(varstream)
-        for line in varstream:
-            values = line.strip().split()
-            loci.append(values)
-    loci.sort(key=lambda l: (l[0], int(l[1]), int(l[2])))
-    for n, valuelist in enumerate(loci, 1):
-        chrom, start, end, source, *idlist = valuelist
-        newid = 'MHDBM{:06d}'.format(n)
-        yield [newid, 'GRCh38', chrom, start, end, source]
-        for xref in idlist:
-            yield [xref, 'marker', newid]
+        name, xref, rsidlist = line.strip().split('\t')
+        rsids = rsidlist.split(,)
+        offsets = [rsidcoords[rsid][1] for rsid in rsids]
+        offsets, rsids = zip(*sorted(offsets, rsids))
+        chrom = rsidcoords[rsids[0]][0]
+        yield name, chrom, offsets, rsids
