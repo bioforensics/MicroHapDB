9a0c78b3c6375c47954a4f1c81e5a0b5d9adf55b
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2019-10-11 12:52:11 -0400

    Linköping markers

---
+++ dbbuild/sources/staadig2019/README.md
@@ -0,0 +1,2 @@
+marker-variants.tsv: exported the "Position" tab of the "Microhaplotype positions and frequences.xlsx" spreadsheet to TSV.
+allele-frequencies.tsv: exported the "Frequencies" tab of the "Microhaplotype positions and frequences.xlsx" spreadsheet to TSV.
---
+++ dbbuild/sources/staadig2019/utils.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2018, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapDB (http://github.com/bioforensics/microhapdb)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import builtins
+from collections import defaultdict
+from gzip import open as gzopen
+from re import findall, search
+import sys
+
+
+def smartopen(filename, mode):
+    """Smart file handler
+
+    Determines whether to create a compressed or un-compressed file handle
+    based on filename extension.
+    """
+    if mode not in ('r', 'w'):
+        raise ValueError('invalid mode "{}"'.format(mode))
+    if filename in ['-', None]:
+        filehandle = sys.stdin if mode == 'r' else sys.stdout
+        return filehandle
+    openfunc = builtins.open
+    if filename.endswith('.gz'):
+        openfunc = gzopen
+        mode += 't'
+    return openfunc(filename, mode)
+
+
+def rename_marker(oldname, chrom):
+    return 'mh{chrnum:02d}AT-{serial:s}'.format(chrnum=int(chrom[3:]), serial=oldname[2:4])
+
+
+def linköping_marker_coords(vcf, mapping):
+    rsidcoords = dict()
+    for line in vcf:
+        if line.startswith('#'):
+            continue
+        chrnum, posstr, rsid, *values = line.strip().split()
+        chrom = chrnum if chrnum.startswith('chr') else 'chr' + chrnum
+        pos = int(posstr) - 1
+        rsidcoords[rsid] = (chrom, pos)
+
+    marker_rsids = defaultdict(list)
+    next(mapping)
+    for line in mapping:
+        marker, rsid, *values = line.strip().split('\t')
+        marker_rsids[marker].append(rsid)
+
+    for marker, rsids in marker_rsids.items():
+        positions = [rsidcoords[r][1] for r in rsids]
+        assert positions == sorted(positions)
+        chrom = rsidcoords[rsids[0]][0]
+        name = rename_marker(marker, chrom)
+        yield name, chrom, positions, rsids
