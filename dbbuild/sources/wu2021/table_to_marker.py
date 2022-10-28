234de7ae6df1fb4c8ea963a2845328af3a2f62ce
Daniel Standage <daniel.standage@st.dhs.gov>
2022-10-28 16:50:07 -0400

    Marker definitions

---
+++ dbbuild/sources/wu2021/table_to_marker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+table = pd.read_csv("tables2.txt", sep="\t", skiprows=3, header=None)
+newtable = list()
+for i, row in table.iterrows():
+    newrow = (
+        row[1],
+        None,
+        row[7],
+        row[0],
+        None,
+        None,
+        row[8],
+    )
+    newtable.append(newrow)
+colnames = ["Name", "Xref", "NumVars", "Chrom", "OffsetsHg37", "OffsetsHg38", "VarRef"]
+markers = pd.DataFrame(newtable, columns=colnames)
+markers.to_csv("marker.tsv", sep="\t", index=False)
